32140a30d2525647ededdbbec25275d4cf641259
Lenon Lencina <lenonlara1@gmail.com>
2023-06-02 19:13:14 -0300

    [WIP]-[ADD]: finalizado visual da tela de login e início do layout da tela home

--- src/app/class/page.js
+++ src/app/class/page.js
@@ -1,7 +1,24 @@
 export default function () {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <p className="text-zinc-50">Hello class</p>
+    <div className="flex items-center justify-center">
+      <div className="bg-violet-500 p-6 w-full m-0 flex items-center gap-20  px-44">
+        <input
+          className="flex-1 rounded-full p-3 bg-violet-100 border shadow-sm  focus:outline-none focus:border-violet-500 focus:ring-violet-500"
+          type="text"
+          name="search"
+          placeholder="pesquisar por curso"
+        />
+        <div className="flex items-center gap-4">
+          <div className="hover:bg-violet-600 transition ease-out duration-150s cursor-pointer p-4 w-max h-max rounded">
+            <p className="text-white">Cursos</p>
+          </div>
+          <div className="hover:bg-violet-600 transition ease-out duration-150s cursor-pointer p-4 w-max h-max rounded">
+            <p className="text-white">Seus cursos</p>
+          </div>
+
+          <div className="bg-violet-900 rounded-full p-6"></div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://kit.fontawesome.com/0212a5fcb5.js"></script>
+      </head>
       <body className="">{children}</body>
     </html>
   );
